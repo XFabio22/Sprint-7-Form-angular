40cedd35f1641bbdc5a79ca7a504e8eea46bcfe9
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2022-09-14 19:20:48 +0200

    corrigiendo

--- Form/src/app/panell/panell.component.ts
+++ Form/src/app/panell/panell.component.ts
@@ -11,55 +11,47 @@ export class PanellComponent  {
 
   constructor(private fb:FormBuilder , private PreciosService:PreciosService ) { }
   myPanellForm :FormGroup = this.fb.group({
-    paginas:[0,[Validators.required , Validators.min(0)]],
-    idiomas:[0,[Validators.required , Validators.min(0)]]
+    paginas:[1,[Validators.required , Validators.pattern(/^[1-9]\d*$/)]],
+    idiomas:[1,[Validators.required , Validators.pattern(/^[1-9]\d*$/)]]
   })
-  opiones={
-    paginas:0,
-    idiomas:0
-  }
+
+  numPaginas: number = 1;
+  numIdiomas: number = 1;
   
   multiValor:number = 0 ;
 
-  sumar(valor:number,campo:string){
+  sumar(campo:string){
     if( campo === "paginas"){
-
-      this.opiones.paginas += valor;
-
-      this.myPanellForm.controls[campo].setValue(this.opiones.paginas);
-   
-      
+      this.numPaginas ++;
     }else  if( campo === "idiomas"){
-      this.opiones.idiomas += valor;
-      this.myPanellForm.controls[campo].setValue(this.opiones.idiomas);
-      
+      this.numIdiomas ++;
+ 
     }
-    console.log(this.opiones);
-  
+
+
   }
 
   restar(valor:number,campo:string){
     
-    if( campo === "paginas"){
-      this.opiones.paginas -= valor;
-      this.myPanellForm.controls[campo].setValue(this.opiones.paginas);   
-      
-    }else  if( campo === "idiomas"){
-      this.opiones.idiomas -= valor;
-      this.myPanellForm.controls[campo].setValue(this.opiones.idiomas);
-     
+    if( valor > 1 && campo === "paginas" ){
+      this.numPaginas --;     
     }
-    console.log(this.opiones)
+    else if( valor > 1 && campo === "idiomas"){
+      this.numIdiomas ++;
+
+    }
+   
+
   
   }
 
 
-  multiValores(){
-    this.multiValor = (this.opiones.paginas * this.opiones.idiomas * 30)
-    console.log(this.multiValor)
-    this.PreciosService.precioTotalGlobal += this.multiValor
+  // multiValores(){
+  //   this.multiValor = (this.opiones.paginas  + this.opiones.idiomas) * 30
+  //   console.log(this.multiValor)
+  //   this.PreciosService.precioTotalGlobal += this.multiValor
 
-  }
+  // }
   //tareas hacer que sume y reste al campo correcto y que el calor sea igual al de el input
   //preuba a crar una array dentro de el objeto para sumar y resatr indeendiente 
 }
